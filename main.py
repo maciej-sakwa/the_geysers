11794e9fae62e94f5b6c43fc8956c0ec6795834c
maciej-sakwa <maciej.sakwa@gmail.com>
2022-12-16 16:38:30 +0100

    Added data clustering short function

--- main.py
+++ main.py
@@ -30,15 +30,21 @@ if __name__ == '__main__':
     # final_dataset = data_preparation.compile_dataset(catalogue_geysers_indexed)
     # final_dataset.to_csv('../data/results_indexed_20062016_2km.csv')
     catalogue_geysers = pd.read_csv('../data/results_indexed_20062016_2km.csv')
-    catalogue_geysers['index_1D'] = catalogue_geysers.apply(lambda row: data_preparation.index1d(row, lat_max, long_max),
+    
+    # Add 1D index to each record of the density dataset
+    lat_max = catalogue_geysers['lat_id'].max()
+    long_max = catalogue_geysers['long_id'].max()
+    catalogue_geysers['index_1D'] = catalogue_geysers.apply(lambda row: data_preparation.index_1d(row, lat_max, long_max),
                                                             axis=1)
+    catalogue_geysers = catalogue_geysers.sort_values(by=['index_1D'])
 
     # time series generation
 
     # time_series = data_preparation.time_series(catalogue_geysers)
     # np.savetxt("../data/time_series_2km.csv", time_series, delimiter=",")
-
-    # timeseries = np.array(pd.read_csv('../data/time_series_2km.csv'))
+    with open('../data/mat_distances_norm.pkl', 'rb') as pickle_file:
+        mat_distances = pickle.load(pickle_file)
+    timeseries = np.array(pd.read_csv('../data/time_series.csv'))
 
     # cc_single = GPUTensorCC(timeseries)
     # single_ccm_matrix, lag_matrix = cc_single.get_result()
@@ -47,13 +53,24 @@ if __name__ == '__main__':
     with open('../data/ccm.csv', 'rb') as pickle_file:
         single_ccm_matrix = pickle.load(pickle_file)
 
+
+    # Fill nan with 1 - the nan values are given for 0 value time series 
     single_ccm_matrix = np.nan_to_num(single_ccm_matrix, nan=1)
-    labels = clustering.perform_hac(single_ccm_matrix)
-    # print(labels)
+    
+    # Perform HAC clustering - save results as PD DF
+    labels = clustering.perform_hac(mat_distances)
+    unique_labels = np.unique(labels)
     cluster_labels = pd.DataFrame(data=labels, columns = ['cluster'])
-    print(cluster_labels.head())
-    catalogue_geysers_clustered = catalogue_geysers.merge(cluster_labels, )
-
-    # plt.show()
 
 
+    # Merge results with original DF
+    catalogue_geysers_clustered = catalogue_geysers.merge(cluster_labels, left_on='index_1D', right_index=True)
+    print(unique_labels)
+    for clust in unique_labels:
+        time_series_cluster = timeseries[labels == clust, :]
+        for i in range(5, 15):
+            plt.plot(time_series_cluster[i])
+        plt.xlabel('Observation month')
+        plt.ylabel('Density [per node]')
+        plt.title('Example time series for cluster '+str(clust))
+        plt.show()
