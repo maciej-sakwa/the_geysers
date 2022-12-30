e53c146e6a68244b0de60daec0de11abf86c0f2c
maciej-sakwa <maciej.sakwa@gmail.com>
2022-12-30 17:01:52 +0100

    ICA and PCA implementation added to clustering.py

--- clustering.py
+++ clustering.py
@@ -1,5 +1,6 @@
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import calinski_harabasz_score, silhouette_score
+from sklearn.decomposition import FastICA, PCA
 from scipy.cluster import hierarchy
 from scipy.spatial import distance
 import numpy as np
@@ -57,3 +58,119 @@ def perform_hac(matrix, method='ward', threshold=0.15, n_cluster=5, criterion='d
     print("Score: {}".format(sil))
 
     return labels, Z
+
+
+def perform_ica(time_series, labels, n_components=2, main_clusters=[4,5]):
+    """
+
+
+    """
+
+    if not n_components == len(main_clusters):
+        print('Error! Number of components mismatched with chosen clusters.')
+        return
+
+    # Extract unique labels from labels
+    arr_labels = np.array(labels['cluster'])
+    unique_labels = np.unique(arr_labels)
+
+    # Pre define averages matrix
+    averages = []
+    averages_ica = []
+
+    # Calculate normalized mean for all clusters
+    for ul in unique_labels:
+
+        # Extract and transform the data from time series dataset
+        # Calculate a normalized average for members of a given cluster
+        time_extract = time_series[arr_labels == ul]
+        time_extract_norm = time_extract.T / np.max(time_extract, axis=1)
+        time_norm = np.average(time_extract_norm.T, axis=0)
+        # Append to averages list
+        averages.append(time_norm)
+
+
+    averages_arr = np.array(averages)
+    averages = np.delete(averages_arr, 0, 1)
+
+    # Extract the chosen clusters
+    for x in main_clusters:
+        index = x-1
+        averages_ica.append(averages[index])
+
+
+
+    transformer = FastICA(n_components=2, random_state=0, whiten='arbitrary-variance')
+    averages_transformed = transformer.fit_transform(np.array(averages_ica))
+    print('Result')
+    print(averages_transformed)
+    print('Components')
+    print(transformer.components_)
+    print('Mixing matrix')
+    print(transformer.mixing_)
+
+    components = transformer.components_
+    for c in range(len(components)):
+        plt.plot(components[c], label='Components {}'.format(c+1))
+
+    # plt.yscale('log')
+    plt.legend()
+    plt.show()
+
+    for c in range(len(components)):
+        plt.plot(averages_transformed[c], label='Components {}'.format(c+1))
+    # plt.yscale('log')
+    plt.legend()
+    plt.show()
+
+    a=1
+
+
+def perform_pca(time_series, labels, n_components=2, main_clusters=[4,5]):
+    """
+
+
+    """
+
+    if not n_components == len(main_clusters):
+        print('Error! Number of components mismatched with chosen clusters.')
+        return
+
+    # Extract unique labels from labels
+    arr_labels = np.array(labels['cluster'])
+    unique_labels = np.unique(arr_labels)
+
+    # Pre define averages matrix
+    averages = []
+    averages_ica = []
+
+    # Calculate normalized mean for all clusters
+    for ul in unique_labels:
+
+        # Extract and transform the data from time series dataset
+        # Calculate a normalized average for members of a given cluster
+        time_extract = time_series[arr_labels == ul]
+        time_extract_norm = time_extract.T / np.max(time_extract, axis=1)
+        time_norm = np.average(time_extract_norm.T, axis=0)
+        # Append to averages list
+        averages.append(time_norm)
+
+
+    averages_arr = np.array(averages)
+    averages = np.delete(averages_arr, 0, 1)
+
+    # Extract the chosen clusters
+    for x in main_clusters:
+        index = x-1
+        averages_ica.append(averages[index])
+
+
+
+    transformer = PCA(n_components=2, random_state=0, whiten=False)
+    averages_transformed = transformer.fit_transform(np.array(averages_ica))
+    print('Result')
+    print(averages_transformed)
+    print('Components')
+    print(transformer.components_)
+
+    return averages_ica, transformer
