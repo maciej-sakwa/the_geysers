bd288d18f76b6e064cc93322a74bd049f1d569ff
maciej-sakwa <maciej.sakwa@gmail.com>
2022-12-30 17:02:26 +0100

    Simple plots for contours and other lines

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pickle
 import clustering
 
 
-def main(**kwargs):
+def main(generate_dataset=True, load_timeseries=True, do_clustering=True):
 
 
     # Density dataset generation
@@ -56,12 +56,11 @@ def main(**kwargs):
 
 
     # Time series generation
-    if timeseries:
-        time_series = data_preparation.time_series(catalogue_geysers)
-        np.savetxt("../data/time_series_2km.csv", time_series, delimiter=",")
-        with open('../data/mat_distances_norm.pkl', 'rb') as pickle_file:
-            mat_distances = pickle.load(pickle_file)
+    if load_timeseries:
+        # time_series = data_preparation.time_series(catalogue_geysers)
+        # np.savetxt("../data/time_series_2km.csv", time_series, delimiter=",")
         time_series = np.array(pd.read_csv('../data/time_series.csv'))
+        print('time_series - dataset loaded.')
 
     # cc_single = GPUTensorCC(time_series)
     # single_ccm_matrix, lag_matrix = cc_single.get_result()
@@ -71,31 +70,64 @@ def main(**kwargs):
     #     single_ccm_matrix = pickle.load(pickle_file)
     # # Fill nan with 1 - the nan values are given for 0 value time series
     # single_ccm_matrix = np.nan_to_num(single_ccm_matrix, nan=1)
+    with open('../data/mat_distances_norm.pkl', 'rb') as pickle_file:
+        mat_distances = pickle.load(pickle_file)
+        print('mat_distances - dataset loaded.')
     
     # Perform HAC clustering - save results as PD DF
     if do_clustering:
-        labels, linkage = clustering.perform_hac(mat_distances, n_cluster=5, criterion='maxclust')
+        labels, linkage = clustering.perform_hac(mat_distances, n_cluster=5, criterion='maxclust', method='ward')
         unique_labels = np.unique(labels)
         cluster_labels = pd.DataFrame(data=labels, columns=['cluster'])
+        cluster_labels.to_csv('../data/cluster_labels')
+    else:
+        cluster_labels = pd.read_csv('../data/cluster_labels')
+        unique_labels = np.unique(np.array(cluster_labels['cluster']))
 
+    means, pca = clustering.perform_pca(time_series, cluster_labels)
 
     # Merge results with original DF
+    # catalogue_geysers = pd.read_csv('../data/results_20062016.csv')
     # catalogue_geysers_clustered = catalogue_geysers.merge(cluster_labels, left_on='index_1D', right_index=True)
-    # catalogue_geysers_clustered.to_csv('../data/full_results_norm_dist_n_5')
+    # catalogue_geysers_clustered.to_csv('../data/full_results_norm_dist_n_5_test')
+    catalogue_geysers_clustered = pd.read_csv('../data/full_results_norm_dist_n_5')
+    print('catalogue_geysers_clustered - dataset loaded.')
+    injections = pd.read_csv('../data/injections.csv', names=['year', 'month', 'Prati9', 'Prati29', 'sum'])
+    injections = injections[injections.year<2017]
+    injections['month_id'] = injections.apply(lambda row: int((row.year - 2006) * 12 + row.month), axis=1)
 
-    catalogue_geysers = pd.read_csv('../data/full_results_norm_dist_n_5')
-    print('Dataset loaded.')
+    # # Plotting
+    # plotting_geo.plot_cluster(catalogue_geysers_clustered, skip=[1,2,3])
+    # plotting_geo.plot_average(time_series, cluster_labels)
+    # plotting_geo.plot_all(time_series, cluster_labels, cluster=4)
+    # for label in unique_labels:
+    #     plotting_geo.plot_average_single(time_series, cluster_labels, cluster=label)
+    plotting_geo.contour_plot_cluster(catalogue_geysers_clustered, 2)
 
-    # for clust in unique_labels:
-    #     time_series_cluster = time_series[labels == clust, :]
-    #     for i in range(5, 15):
-    #         plt.plot(time_series_cluster[i])
-    #     plt.xlabel('Observation month')
-    #     plt.ylabel('Density [per node]')
-    #     plt.title('Example time series for cluster '+str(clust))
-    #     plt.show()
 
+    # fig, axs = plt.subplots(pca.n_components, sharex='all')
+    # for i, ax in zip(range(pca.n_components), axs.ravel()):
+    #     ax.plot(pca.components_[i], label='Component {}'.format(i+1))
+    #     ax.legend(loc='upper right')
+    # plt.show()
+
+    # plt.plot(injections['month_id'],injections['sum'], label='Sum')
+    # # plt.plot(injections['Prati9'], label='Prati9')
+    # # plt.plot(injections['Prati29'], label='Prati29')
+    # plt.legend()
+    # plt.show()
+    # a=1
+
+    # fig, ax = plt.subplots(5, sharex='all')
+    # ax[0].plot(-pca.components_[0], label='Component {}'.format(i+1))
+    # ax[1].plot(pca.components_[1], label='Component {}'.format(i + 1))
+    # ax[2].plot(injections['month_id'],injections['sum'], label='Sum')
+    # ax[3].plot(means[0], label='Cluster 4')
+    # ax[4].plot(means[1], label='Cluster 5')
+    # plt.legend()
+    # plt.show()
+    # a=1
 
 if __name__ == '__main__':
 
-    main(do_clustering=False, timeseries=False, generate_dataset=False)
+    main(do_clustering=False, load_timeseries=True, generate_dataset=False)
--- plotting_geo.py
+++ plotting_geo.py
@@ -14,6 +14,7 @@ mpl.rcParams['axes.linewidth'] = 1.5
 # mpl.rcParams['ases.spines.right'] = False
 
 
+
 def density(dataset, year, month):
     """
     Plots a 3d graph of seismic activity density in the studied 'cube'
@@ -93,7 +94,111 @@ def contour_plot(density_dataset, d):
         plt.savefig(file_path)
 
 
-def make_gif(frame_folder, depth):
+def make_gif(frame_folder, depth: int):
+    """
+    Make gif out of density contour plots
+
+    :param frame_folder: path to folder containing contour plots
+    :param depth: int, selected depth level
+    :return:
+    """
+
     frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.png")]
     frame_one = frames[0]
     frame_one.save("depth_"+str(depth)+".gif", format="GIF", append_images=frames, save_all=True, duration=200, loop=0)
+
+
+def plot_cluster(dataset, skip=[0]):
+    plt.style.use('ggplot')
+    selected_data = dataset[(dataset['density']>0)]
+    plot_data = selected_data.groupby(['lat', 'long', 'depth'])['cluster'].mean()
+    plot_data = plot_data.reset_index()
+
+    clusters = np.unique(np.array(plot_data['cluster']))
+
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    for c in clusters:
+
+        if c in skip:
+            continue
+
+        cluster_data = plot_data[plot_data['cluster']==c]
+        lat_c = np.array(cluster_data['lat'])
+        long_c = np.array(cluster_data['long'])
+        depth_c = np.array(cluster_data['depth'])
+
+        ax.scatter3D(lat_c, long_c, -depth_c, label="Cluster {}".format(c))
+
+    plt.title('3D cluster graph')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_average(timeseries, labels, skip=[0]):
+    plt.style.use('ggplot')
+    arr_labels = np.array(labels['cluster'])
+    unique_labels = np.unique(arr_labels)
+
+    for ul in unique_labels:
+
+        if c in skip:
+            continue
+
+        time_extract = timeseries[arr_labels == ul]
+        print(len(time_extract))
+        time_extract_norm = time_extract.T / np.max(time_extract, axis=1)
+        time_norm = np.average(time_extract_norm.T, axis=0)
+
+        plt.plot(time_norm, label='Cluster {}'.format(ul))
+
+    plt.legend()
+    plt.title('Averaged normalised time series for created clusters')
+    plt.xlabel('Month')
+    plt.ylabel('Density [p.u.]')
+    plt.show()
+
+
+def plot_average_single(timeseries, labels, cluster):
+    """
+
+    """
+    arr_labels = np.array(labels['cluster'])
+    x = range(timeseries.shape[1])
+
+
+    time_extract = timeseries[arr_labels == cluster]
+    time_extract_norm = time_extract.T / np.max(time_extract, axis=1)
+    time_norm = np.average(time_extract_norm.T, axis=0)
+    std = np.std(time_extract_norm.T, axis=0)
+
+    fig, ax = plt.subplots()
+    ax.plot(time_norm, label='Mean for cluster {}'.format(cluster), c='k')
+    ax.plot(time_norm+std, "k--", label='Standard deviation')
+    ax.plot(time_norm-std, 'k--')
+    ax.fill_between(x, time_norm-std, time_norm+std, color='#56B4E9', alpha=0.4)
+
+    plt.legend()
+    fig.set_size_inches(10, 5)
+    plt.title('Average normalised time series for cluster {}'.format(cluster))
+    plt.xlabel('Observation month')
+    plt.ylabel('Density [p.u.]')
+    plt.show()
+
+
+def contour_plot_cluster(dataset, depth):
+
+    dataset_depth = dataset[dataset['depth'] == depth]
+    cluster_matrix = np.zeros((dataset_depth.lat_id.max() + 1, dataset_depth.long_id.max() + 1))
+
+    for index, row in dataset_depth.iterrows():
+        i = int(row['lat_id'])
+        j = int(row['long_id'])
+        cluster_matrix[i, j] = row.loc['cluster']
+
+    fig, ax = plt.subplots(1, 1)
+    cp = ax.scatter(x=dataset_depth['lat_id'], y=dataset_depth['long_id'], c=dataset_depth['cluster'])
+    plt.colorbar(cp)
+    plt.show()
