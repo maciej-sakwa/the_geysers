d927d7140e3de98e038d8f5f5bc215d6a8af8bda
maciej-sakwa <maciej.sakwa@gmail.com>
2023-02-15 12:13:27 +0100

    Code cleanup and replaced the plot means function

--- plotting_geo.py
+++ plotting_geo.py
@@ -12,36 +12,7 @@ import plotly.graph_objects as go
 #General plot parameters
 mpl.rcParams['font.size'] = 12
 mpl.rcParams['axes.linewidth'] = 1.5
-# mpl.rcParams['axes.spines.top'] = False
-# mpl.rcParams['ases.spines.right'] = False
 
-
-
-def density(dataset, year, month):
-    """
-    Plots a 3d graph of seismic activity density in the studied 'cube'
-
-    :param dataset:    pandas dataframe; contains all the data
-    :param year:    int; the year in which you plot, year has to be contained in dataset
-    :param month:   int; the year in which you plot, year has to be contained in dataset
-    :return:
-    """
-
-    selected_data = dataset[(dataset['year'] == year) & (dataset['month'] == month)]
-    lat = np.array(selected_data['lat_id'])
-    long = np.array(selected_data['long_id'])
-    depth = np.array(selected_data['depth_id'])
-
-
-
-
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    geo_dens = ax.scatter3D(lat, long, depth, c=selected_data['density'])
-    plt.colorbar(geo_dens)
-    plt.title('3D density graph for: ' + str(month) +', ' + str(year))
-    plt.tight_layout()
-    plt.show()
 
 
 def contour_plot(density_dataset, d):
@@ -118,106 +89,53 @@ def plot_cluster(dataset, skip=[0]):
     plt.show()
 
 
-def plot_average(timeseries, labels, skip=[0]):
-    plt.style.use('ggplot')
-    arr_labels = np.array(labels['cluster'])
-    unique_labels = np.unique(arr_labels)
+def plot_means_subplots(time_series, cluster_labels) -> None:
+    """
+    Plots the Means and STD graph of the the chosen domains
 
-    for ul in unique_labels:
+    :param time_series:
+    :param cluster_labels:
 
-        if c in skip:
-            continue
+    """
 
-        time_extract = timeseries[arr_labels == ul]
-        print(len(time_extract))
+    # Parameters and label definitions
+    mpl.rcParams['font.size'] = 14
+    arr_labels = np.array(cluster_labels['cluster'])
+    clusters = [3, 4, 5]
+    legend_labels = ['DTH-A', 'DTH-B', 'DTH-C']
+    years = np.arange(2006, 2017, 1)
+    tick_labels = [f'\'{item[-2:]}' for item in map(str, years)]
+    i = 0
+    x = range(time_series.shape[1])
+
+    # Plot definition
+    fig, axs = plt.subplots(3)
+    for cluster, ax in zip(clusters, axs.ravel()):
+        # Normalization and mean calculation
+        time_extract = time_series[arr_labels == cluster]
         time_extract_norm = time_extract.T / np.max(time_extract, axis=1)
         time_norm = np.average(time_extract_norm.T, axis=0)
+        std = np.std(time_extract_norm.T, axis=0)
 
-        plt.plot(time_norm, label='Cluster {}'.format(ul))
+        # Mean and STD plots with fill between
+        ax.plot(time_norm, label='{}'.format(legend_labels[i]), c='k')
+        ax.plot(time_norm + std, "k--", label='SD')
+        ax.plot(time_norm - std, 'k--')
+        ax.fill_between(x, time_norm - std, time_norm + std, color='#a7c8e2')
+        ax.legend(loc='upper left', framealpha=1)
 
-    plt.legend()
-    plt.title('Averaged normalised time series for created clusters')
-    plt.xlabel('Month')
-    plt.ylabel('Density [p.u.]')
+        # X-axis settings
+        ax.set_xticks(np.arange(0, 121, 12))
+        ax.set_xticklabels(tick_labels)
+        ax.set_xlim([0, 125])
+        i += 1
+
+    # Plot area settings
+    fig.supylabel('Normalized density [p.u.]')
+    fig.set_size_inches(8, 7.5)
+    plt.xlabel('Observation date [y]')
+    plt.tight_layout()
     plt.show()
-
-
-def plot_average_single(timeseries, labels, cluster):
-    """
-
-    """
-    arr_labels = np.array(labels['cluster'])
-    x = range(timeseries.shape[1])
-
-
-    time_extract = timeseries[arr_labels == cluster]
-    time_extract_norm = time_extract.T / np.max(time_extract, axis=1)
-    time_norm = np.average(time_extract_norm.T, axis=0)
-    std = np.std(time_extract_norm.T, axis=0)
-
-    fig, ax = plt.subplots()
-    ax.plot(time_norm, label='Mean for cluster {}'.format(cluster), c='k')
-    ax.plot(time_norm+std, "k--", label='Standard deviation')
-    ax.plot(time_norm-std, 'k--')
-    ax.fill_between(x, time_norm-std, time_norm+std, color='#56B4E9', alpha=0.4)
-
-    plt.legend()
-    fig.set_size_inches(10, 5)
-    plt.title('Average normalised time series for cluster {}'.format(cluster))
-    plt.xlabel('Observation month')
-    plt.ylabel('Density [p.u.]')
-    plt.show()
-
-
-def contour_plot_cluster(dataset, depth):
-
-    dataset_depth = dataset[dataset['depth'] == depth]
-    cluster_matrix = np.zeros((dataset_depth.lat_id.max() + 1, dataset_depth.long_id.max() + 1))
-
-    for index, row in dataset_depth.iterrows():
-        i = int(row['lat_id'])
-        j = int(row['long_id'])
-        cluster_matrix[i, j] = row.loc['cluster']
-
-    fig, ax = plt.subplots(1, 1)
-    cp = ax.scatter(x=dataset_depth['lat_id'], y=dataset_depth['long_id'], c=dataset_depth['cluster'])
-    plt.colorbar(cp)
-    plt.show()
-
-
-    # # Plotting
-    # plotting_geo.plot_cluster(catalogue_geysers_clustered, skip=[1,2,3])
-    # plotting_geo.plot_average(time_series, cluster_labels)
-    # plotting_geo.plot_all(time_series, cluster_labels, cluster=4)
-    # for label in unique_labels:
-    #     plotting_geo.plot_average_single(time_series, cluster_labels, cluster=label)
-    # plotting_geo.contour_plot_cluster(catalogue_geysers_clustered, 2)
-
-    # fig, axs = plt.subplots(pca.n_components, sharex='all')
-    # for i, ax in zip(range(pca.n_components), axs.ravel()):
-    #     ax.plot(pca.components_[i], label='Component {}'.format(i+1))
-    #     ax.legend(loc='upper right')
-    # plt.show()
-
-    # plt.plot(injections['month_id'],injections['sum'], label='Sum')
-    # # plt.plot(injections['Prati9'], label='Prati9')
-    # # plt.plot(injections['Prati29'], label='Prati29')
-    # plt.legend()
-    # plt.show()
-    # a=1
-
-    # fig, ax = plt.subplots(3, sharex='all')
-    # ax[0].plot(-pca.components_[0], label='Component 1')
-    # # ax[0].plot(pca.components_[1], label='Component 2')
-    # ax[0].legend()
-    # ax[1].plot(injections['month_id'], injections['sum'], label='Sum')
-    # ax[1].plot(injections['month_id'], injections['Prati9'], label='Prati9')
-    # ax[1].plot(injections['month_id'], injections['Prati29'], label='Prati29')
-    # ax[1].legend()
-    # ax[2].plot(means[0], label='Cluster 4')
-    # ax[2].plot(means[1], label='Cluster 5')
-    # ax[2].legend()
-    # plt.show()
 
 
 def volume_plot(density_df, geo_bound):
@@ -268,3 +186,39 @@ def volume_plot(density_df, geo_bound):
     ))
 
     fig.show()
+
+
+  # fig, ax = plt.subplots(4)
+    # for i in range(3):
+    #     ax[0].plot(np.array(ind_components.iloc[:, i+1]).T, label='IC: {}'.format(i+1))
+    # ax[0].set_title('Independent Components')
+    # ax[0].legend(loc='upper right')
+    #
+    # ax[1].set_title('Injection Rates')
+    # ax[1].plot(injections_local.month_id, injections_local.injections/injections_local.injections.max(),
+    #            label='Injections_LOCAL')
+    # ax[1].plot(injections_global.month_id, injections_global.injections/injections_global.injections.max(),
+    #            label='Injections_GLOBAL')
+    # ax[1].legend(loc='upper right')
+    #
+    # ax[2].set_title('Injection FFT')
+    # ax[2].plot(xf_local, np.abs(fft_local)[:sample_size_local//2], label='Injections_FFT_LOCAL')
+    # ax[2].plot(xf_global, np.abs(fft_global)[:sample_size_global//2], label='Injections_FFT_GLOBAL')
+    # ax[2].legend(loc='upper right')
+    # years = np.arange(2006,2017,1)
+    # heights = injections_yearly[(injections_yearly['date']>2005) & (injections_yearly['date']<2017)].injections
+    # ax[3].bar(years, height=heights)
+    # ax[3].set_title('Yeary Injections')
+    #
+    # plt.show()
+
+
+
+    # n_clust=np.arange(3, 11, 1)
+    # silhuette = [0.41, 0.1675, 0.4477, 0.439, 0.425084, 0.341536, 0.335546, 0.333744]
+    #
+    # fig_bar = plt.bar(n_clust, silhuette, width = 0.95, color='#a7c8e2')
+    # plt.ylabel('Silhouette coefficient')
+    # plt.xlabel('Number of clusters')
+    # plt.tight_layout
+    # plt.show()
